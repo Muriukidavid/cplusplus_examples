028a5b8697831e725287154dce9c59a41f673bdf
muriukidavid <karfes@gmail.com>
2014-10-30 08:38:43 +0300

    add a systemc example

---
+++ hello.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "systemc.h"
+#include "hello.h"
+
+using namespace std;
+
+int sc_main(int argc, char* argv[]){
+
+sc_report_handler::set_actions("/IEEE_Std_1666/deprecated", SC_DO_NOTHING);
+cout<<"the set time resolution is: "<<sc_get_default_time_unit()<<endl;
+
+test mytest("mytest");
+sc_start();
+return 0;
+}
