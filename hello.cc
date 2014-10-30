b3046e5f28d3183d07e5b343801709a5a422fc25
GathuBoswell <gathudev@gmail.com>
2014-10-30 11:48:11 +0300

    defined start simulation duration and edited sc_report_info first argument

--- hello.cc
+++ hello.cc
@@ -10,6 +10,6 @@ sc_report_handler::set_actions("/IEEE_Std_1666/deprecated", SC_DO_NOTHING);
 cout<<"the set time resolution is: "<<sc_get_default_time_unit()<<endl;
 
 test mytest("mytest");
-sc_start();
+sc_start(50,SC_NS);
 return 0;
 }
