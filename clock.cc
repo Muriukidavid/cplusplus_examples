af631ce430f2df30baa60bb3d26fdb40badde64e
Hillary <kiplagathillary22@gmail.com>
2015-10-06 09:22:15 +0300

    EDDED 24 HOUR CLOCK FUNCTION

--- clock.cc
+++ clock.cc
@@ -1,44 +1,53 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-class Time{
-	public:
-		void setTime(int sethours, int setmins, int setsecs){
-			hours = sethours;
-			mins = setmins;
-			secs=setsecs;
-		}
-
-		void displayTime(void){
-			cout<<hours<<":"<<mins<<":"<<secs<<ampm<<"\n";
-		}
-
-	protected:	int hours;
-			 	int mins;
-				int secs;
-				char ampm[3];
-};
-
-class Clock: public Time{
-	public:
-		void setAMPM(void){
-			if(hours>=12){
-				if(hours>12)
-					hours=hours-12;
-				std::string str (" PM");
-				str.copy(ampm, 3, 0);
-			}else{
-				std::string str (" AM");
-				str.copy(ampm, 3, 0);
-			}
-		}
-};
-
-int main(){
-	Clock clk;
-	clk.setTime(13,25,40);
-	clk.setAMPM();
-	clk.displayTime();
-	return 0;
-}
+ #include<iostream>
+ #include<string>
+ using namespace std;
+ 
+ class Time{
+ 	public:
+ 		void setTime(int sethours, int setmins, int setsecs){
+ 			hours = sethours;
+ 			mins = setmins;
+ 			secs=setsecs;
+ 		}
+ 
+ 		void displayTime(void){
+-			cout<<hours<<":"<<mins<<":"<<secs<<ampm<<"\n";
++			cout<<hours<<":"<<mins<<":"<<secs<<ampm<<"\tin 12-hour clock"<<"\n";
+ 		}
++		
++                void displayTime24Hours(void){
++                	cout<<zero<<hours<<":"<<mins<<":"<<secs<<"\tin 24-hour clock"<<endl;
++                }  
+ 
+ 	protected:	int hours;
+ 			 	int mins;
+ 				int secs;
+ 				char ampm[3];
++				char zero[1];
+ };
+ 
+ class Clock: public Time{
+@@ -33,11 +38,22 @@ class Clock: public Time{
+ 				str.copy(ampm, 3, 0);
+ 			}
+ 		}
++//		void setZero(void){
++//			if(hours<=9){
++//				std::string str("0");
++//				str.copy(zero, 1, 0);
++//			}
++//			else{
++//			
++//			}
++//		}
+ };
+ 
+ int main(){
+ 	Clock clk;
+ 	clk.setTime(13,25,40);
++//	clk.setZero();
++	clk.displayTime24Hours();
+ 	clk.setAMPM();
+ 	clk.displayTime();
+ 	return 0;
+ }
