d6b6bc8841c3dea682f44093907e9d1b070d665c
mulan-94 <amwayilexy.me@gmail.com>
2015-10-08 07:36:42 +0300

    changed atm to include deposit and other things

--- atm.cc
+++ atm.cc
@@ -1,36 +1,10 @@
-#include <iostream>
+#include<iostream>
 #include "atm.h"
 using namespace std;
+
 int main ()
 {
-int PIN,depo,c,d;
-account me;
-	cout<<"Please enter your current PIN: "<<endl;
-	cin>>PIN;
-	
-c=me.enterPin(PIN);
-if (c==0){
-
- d=me.dispOptions();
- 	
- 	if (d==1){
- 
- 	//me.displayBalance();
-
-		cout<<"Enter the amount you want to deposit: "<<endl;
-		cin>>depo;
-	
- 		me.setBalance (depo);
- 		me.displayBalance();
- 		}
- 		
- 			else{
-				me.displayBalance();		
- 			}
-}
-	else
-	cout<<"Your account has been blocked!"<<endl;
-
+account myAccount;
+myAccount.enterPin();
 return 0;
- 
- }
+}
